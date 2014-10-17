5d93b8f78798461d24da325c0d499b3b216a6f75
Ryan Bales <ryankbales@gmail.com>
2014-10-16 20:43:32 -0700

    getting card values

--- the_game.rb
+++ the_game.rb
@@ -28,14 +28,14 @@ require 'pry'
 
 #create the deck of card_suits
 deck_hash = {two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10, jack: 10, queen: 10, king: 10, acehigh: 11, acelow: 1}
-card_face = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
+card_face = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
 card_suits = ["hearts", "dimonds", "clubs", "spades"]
 deck = card_face.product(card_suits)
 
 puts "Lets play Blackjack"
 shuffled_deck = deck.shuffle
 
-players_cards = []
+player_cards = []
 dealer_cards = []
 
 def deal_two_cards_each(deck, player, dealer)
@@ -45,5 +45,24 @@ def deal_two_cards_each(deck, player, dealer)
   end
 end
 
-deal_two_cards_each(shuffled_deck, players_cards, dealer_cards)
-binding.pry
+def get_hand_value(player, values)
+  player_card_values = []
+  player.each do |card|
+    values.each do |k, v|
+      if card[0] == k.to_s
+        player_card_values << v
+      end
+    end
+  end
+  player_hand = (player_card_values[0] + player_card_values[1])
+  return player_hand
+end
+
+deal_two_cards_each(shuffled_deck, player_cards, dealer_cards)
+
+begin
+  #check each hand, place and add values
+  player_cards = get_hand_value(player_cards, deck_hash)
+  dealer_cards = get_hand_value(dealer_cards, deck_hash)
+
+end #if winner || empty array
